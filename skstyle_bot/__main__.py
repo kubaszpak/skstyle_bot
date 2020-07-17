0458d73cdb4171801b55924575bf8880d3851b17
kubaszpak <jakub.szpak.it@gmail.com>
2020-07-17 03:53:18 +0200

    Made it usable for other stores, also usable in minimized mode now, only saving to file left

--- skstyle_bot/__main__.py
+++ skstyle_bot/__main__.py
@@ -5,14 +5,15 @@ from selenium import webdriver
 import os
 
 class Skstyle:
-    def __init__(self,username,password):
+    def __init__(self,username,password,domain):
+        self.domain = domain
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--incognito")
         chrome_options.add_experimental_option("detach", True)
         chrome_options.add_argument("--start-maximized")
         self.driver = webdriver.Chrome(options=chrome_options)
         # self.driver.minimize_window()
-        self.driver.get("https://skstyle.pl/admin/login.php")
+        self.driver.get(f"https://{domain}/admin/login.php")
         sleep(1)
         self.driver.find_element_by_xpath("//input[@name='email']").send_keys(username)
         sleep(1)
@@ -21,29 +22,29 @@ class Skstyle:
         sleep(1)
         self.driver.find_element_by_xpath("//input[@value='Zaloguj']").click()
         sleep(1)
-        self.driver.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/a").click()
+        # this part was supposed to enable running also in minimized window
+        try:
+            self.driver.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/a").click()
+        except Exception:
+            # this part lets you change the size of window while the script is on
+            fa_fa_bars = self.driver.find_element_by_xpath("/html/body/header/nav/ul/li[2]/a")
+            fa_fa_bars.click()
+            sleep(1)
+            orders_and_clients = fa_fa_bars.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/a/span[2]")
+            orders_and_clients.click()
+            sleep(1)
+            orders = orders_and_clients.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/ul/li[1]/a/span")
+            orders.click()
+            sleep(1)
+            orders_list = orders.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/ul/li[1]/ul/li[1]/a")
+            orders_list.click()
+            sleep(1)
+        except:
+            # but you can also achieve the same effect with 
+            # ugly way of doing the same thing
+            self.driver.get(f"https://{domain}/admin/orders.php")
+       
 
-        """ comments are an attempt to implement mobile minimized window"""
-
-        # try:
-        #     self.driver.find_element_by_xpath("//*[@id='helpBanner']/div[2]/div/a").click()
-        # except:
-        #     pass
-        # self.driver.find_element_by_xpath("///html/body/div[2]/div[1]/div[2]/div[2]/ul/li[1]/a[@href = 'https://skstyle.pl/admin/orders.php']").click()
-        # sleep(1)
-        # try:
-        # except:
-        #     self.driver.find_element_by_xpath("/html/body/header/nav/ul/li[2]/a").click()
-            # sleep(1)
-            # self.driver.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/a").click()
-            # sleep(1)
-            # self.driver.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/a/span[2]").click()
-            # sleep(1)
-            # self.driver.find_element_by_xpath("//a[@href='https://skstyle.pl/admin/orders.php']").click()
-            # sleep(1)
-        # finally:
-        #     sleep(5)
-        #     self.driver.close()
         
 
     def is_there_new_order(self):
@@ -58,30 +59,40 @@ class Skstyle:
             f.write(str(latest_order))
             #//div[@class='widthId']
             f.close()
-            return last_printed_order,latest_order
+            return last_printed_order
         else:
             f = open(file_to_open, "w")
             latest_order = (self.driver.find_element_by_xpath("/html/body/div[2]/form[2]/section[1]/div[2]/div[1]/div[2]").text)
             f.write(latest_order)
             f.close()
-            return None, latest_order
+            return None
 
-    def handle_orders(self,last_printed_order,latest_order):
+    def handle_orders(self,last_printed_order):
         order_section = self.driver.find_element_by_xpath("/html/body/div[2]/form[2]/section[1]/div[2]")
-        orders = order_section.find_elements_by_xpath("//div[@class='row new']")
-        orders_to_print = {}
-        for order in orders:
-            order_number = int(order.find_element_by_xpath("//div[@class='widthId']").text)
-            if(order_number >= last_printed_order and order_number < latest_order):
-                orders_to_print[order_number]=order
-        print(orders_to_print)
+        orders = order_section.find_elements_by_xpath("./div[starts-with(@class,'row ')]")
+        # for order in orders:
+            # order_number = int(order.find_element_by_xpath("./div[@class='widthId']").text)
+            # print(order_number)
+            # if(order_number > last_printed_order):
+                # print(order.find_element_by_xpath(".//a[starts-with(@href,'https://{domain}/admin/orders.php?page=1&oID')]").text)
+                # order_to_file(order)
+        order_number = int(orders[0].find_element_by_xpath("./div[@class='widthId']").text)
+        # order_to_file(orders[0])
+        self.driver.get(f"https://{self.domain}/admin/orders.php?page=1&oID={order_number}&action=edit&ro=1")
+
+    def close(self):
+        self.driver.quit()
 
 
+def order_to_file(order,domain):
+    order.find_element_by_xpath(f".//a[starts-with(@href,'https://{domain}/admin/orders.php?page=1&oID=')]").click()
 
 def main():
-    my_bot = Skstyle(input("Your login: "),input("Your password: "))
-    # my_bot = Skstyle(secrets.username,secrets.password)
-    last_printed_order,latest_order = my_bot.is_there_new_order()
+    driver = Skstyle(input("Your login: "),input("Your password: "),input("Your domain: "))
+    # driver = Skstyle(secrets.username,secrets.password)
+    last_printed_order = driver.is_there_new_order()
     if(last_printed_order!=None):
-        my_bot.handle_orders(last_printed_order,latest_order)
+        driver.handle_orders(last_printed_order)
+    driver.close()
+
 

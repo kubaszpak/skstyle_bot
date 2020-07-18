2804a371e8f4fc0f200ebbc8dfb472dbce6daa23
kubaszpak <jakub.szpak.it@gmail.com>
2020-07-18 05:15:07 +0200

    Method to handle order, bug to fix (page forgets orders after refreshing)

--- skstyle_bot/__main__.py
+++ skstyle_bot/__main__.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from time import sleep
 from selenium import webdriver
 # from skstyle_bot import secrets
+import selenium.common.exceptions
 import os
 
 class Skstyle:
@@ -12,6 +13,9 @@ class Skstyle:
         chrome_options.add_experimental_option("detach", True)
         chrome_options.add_argument("--start-maximized")
         self.driver = webdriver.Chrome(options=chrome_options)
+
+
+        # logging
         # self.driver.minimize_window()
         self.driver.get(f"https://{domain}/admin/login.php")
         sleep(1)
@@ -22,32 +26,38 @@ class Skstyle:
         sleep(1)
         self.driver.find_element_by_xpath("//input[@value='Zaloguj']").click()
         sleep(1)
+
+
+        # getting to order section
         # this part was supposed to enable running also in minimized window
         try:
             self.driver.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/a").click()
         except Exception:
-            # this part lets you change the size of window while the script is on
-            fa_fa_bars = self.driver.find_element_by_xpath("/html/body/header/nav/ul/li[2]/a")
-            fa_fa_bars.click()
-            sleep(1)
-            orders_and_clients = fa_fa_bars.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/a/span[2]")
-            orders_and_clients.click()
-            sleep(1)
-            orders = orders_and_clients.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/ul/li[1]/a/span")
-            orders.click()
-            sleep(1)
-            orders_list = orders.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/ul/li[1]/ul/li[1]/a")
-            orders_list.click()
-            sleep(1)
-        except:
-            # but you can also achieve the same effect with 
-            # ugly way of doing the same thing
-            self.driver.get(f"https://{domain}/admin/orders.php")
-       
+            try:
+                # this part lets you change the size of window while the script is on
+                fa_fa_bars = self.driver.find_element_by_xpath("/html/body/header/nav/ul/li[2]/a")
+                fa_fa_bars.click()
+                sleep(1)
+                orders_and_clients = fa_fa_bars.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/a/span[2]")
+                orders_and_clients.click()
+                sleep(1)
+                orders = orders_and_clients.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/ul/li[1]/a/span")
+                orders.click()
+                sleep(1)
+                orders_list = orders.find_element_by_xpath("/html/body/header/nav/div/div[1]/ul/li[2]/ul/li[1]/ul/li[1]/a")
+                orders_list.click()
+                sleep(1)
+            except selenium.common.exceptions.ElementNotInteractableException:
+                # but you can also achieve the same effect with 
+                # it is an ugly way of doing the same thing
+                self.driver.get(f"https://{domain}/admin/orders.php")
+        
 
         
 
     def is_there_new_order(self):
+        # checks if there is a new order
+        # if you run it for the first time if returns None
         data_folder = Path('skstyle_bot/')
         file_to_open = data_folder / 'last_order.txt'
         if(file_to_open.is_file() and os.stat(file_to_open).st_size != 0):
@@ -67,25 +77,52 @@ class Skstyle:
             f.close()
             return None
 
+
+
     def handle_orders(self,last_printed_order):
         order_section = self.driver.find_element_by_xpath("/html/body/div[2]/form[2]/section[1]/div[2]")
         orders = order_section.find_elements_by_xpath("./div[starts-with(@class,'row ')]")
-        # for order in orders:
-            # order_number = int(order.find_element_by_xpath("./div[@class='widthId']").text)
-            # print(order_number)
-            # if(order_number > last_printed_order):
-                # print(order.find_element_by_xpath(".//a[starts-with(@href,'https://{domain}/admin/orders.php?page=1&oID')]").text)
-                # order_to_file(order)
-        order_number = int(orders[0].find_element_by_xpath("./div[@class='widthId']").text)
-        # order_to_file(orders[0])
-        self.driver.get(f"https://{self.domain}/admin/orders.php?page=1&oID={order_number}&action=edit&ro=1")
+        for order in orders:
+            order_number = int(order.find_element_by_xpath("./div[@class='widthId']").text)
+            if(order_number > 222):
+                order_to_file(self.driver,order,order_number,self.domain)
 
     def close(self):
         self.driver.quit()
 
 
-def order_to_file(order,domain):
-    order.find_element_by_xpath(f".//a[starts-with(@href,'https://{domain}/admin/orders.php?page=1&oID=')]").click()
+def order_to_file(webdriver,order,order_number,domain):
+    try:
+        order.find_element_by_xpath(f".//a[starts-with(@href,'https://{domain}/admin/orders.php?page=1&oID=')]").click()
+        order_data = webdriver.find_element_by_xpath("//a[@href='#wysylka']")
+        order_data.click()
+    except:
+        webdriver.get(f"https://{domain}/admin/orders.php?page=1&oID={order_number}&action=edit&ro=1#wysylka")
+
+
+    receiver_form_address_group = webdriver.find_element_by_xpath("//*[@id='receiverForm']/div")
+    rows_receive = receiver_form_address_group.find_elements_by_xpath("./div[starts-with(@class,'row')]")
+    contents = []
+    for row in rows_receive:
+        try:
+            contents.append(row.find_element_by_xpath("./div[@class='content']"))
+        except:
+            continue
+
+    shipment_form_adrress_group = webdriver.find_element_by_xpath("//*[@id='shipmentForm']/div")
+    rows_ship = shipment_form_adrress_group.find_elements_by_xpath("./div[starts-with(@class,'row')]")
+    for row in rows_ship:
+        try:
+            contents.append(row.find_element_by_xpath("./div[@class='content']"))
+        except:
+            continue
+    final_list = [content.text for content in contents if(content.text != '')]
+    final_list.insert(0,str(order_number))
+    print(final_list)
+    
+    webdriver.get("https://skstyle.pl/admin/orders.php")
+    sleep(2)
+    
 
 def main():
     driver = Skstyle(input("Your login: "),input("Your password: "),input("Your domain: "))
